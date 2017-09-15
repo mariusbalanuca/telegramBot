ec50af426789b0a8eea59226c6da0af4d32a8e24
Marius Balanuca <marius.balanuca@gmail.com>
2017-09-15 10:10:38 +0300

    Update telegramBot.py

--- telegramBot.py
+++ telegramBot.py
@@ -1,7 +1,7 @@
 import json 
 import requests
 import time
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import re
 
 TOKEN = "413360913:AAFkA4qDK3ht2NGEeBdPshhfYbHLz3rxsAE"
@@ -15,8 +15,8 @@ def get_url(url):
 def weather(oras):
     site = "http://www.google.ro/search?q=weather+" + oras
     hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib2.Request(site,headers=hdr)
-    page = urllib2.urlopen(req)
+    req = urllib.request.Request(site,headers=hdr)
+    page = urllib.request.urlopen(req)
     from bs4 import BeautifulSoup
     soup = BeautifulSoup(page,"html.parser")
     soup.prettify("utf-8")
@@ -60,7 +60,7 @@ def send_message(text, chat_id):
             text = text.split()[1]
         url = URL + "sendMessage?text={}&chat_id={}".format(weather(text).encode('utf-8'), chat_id)
     else:
-        url = URL + "sendMessage?text={}&chat_id={}".format("I`m stupid now...i don`t know much", chat_id)
+        url = URL + "sendMessage?text={}&chat_id={}".format("I`m stupid now...i don`t know much".encode('utf-8'), chat_id)
     get_url(url)
     
 def get_last_update_id(updates):
@@ -90,3 +90,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
