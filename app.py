05b738ceb0dd9c7c703cc5617c48db7401d1dc76
Marius Balanuca <marius.balanuca@gmail.com>
2017-10-18 15:20:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import json 
+import json
 import requests
 import time
 import urllib2
@@ -9,59 +9,80 @@ TOKEN = "413360913:AAFkA4qDK3ht2NGEeBdPshhfYbHLz3rxsAE"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 def flipCoin():
-  foo = ["tails", "heads"]
-  return random.choice(foo)
-  
+    foo = ["tails", "heads"]
+    return random.choice(foo)
+
+
 def rollDice():
-  dice=[1,2,3,4,5,6]
-  return random.choice(dice)
-      
+    dice = [1, 2, 3, 4, 5, 6]
+    return random.choice(dice)
+
+
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
-    
+
+
 def weather(oras):
-    site = "http://api.openweathermap.org/data/2.5/weather?q="+oras+"&appid=631cf33df60fe496c2a9d57992fa99ba"
+    site = "http://api.openweathermap.org/data/2.5/weather?q=" + oras + "&appid=631cf33df60fe496c2a9d57992fa99ba"
     data = json.load(urllib2.urlopen(site))
     return oras + ": %d C - %s" % (int(data["main"]["temp"] - 273.15), data["weather"][0]["main"])
+
 
 def score(team):
     text = "Team not found or doesn`t have a match today"
     site = "http://www.tablesleague.com/livescores/"
     hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib2.Request(site,headers=hdr)
+    req = urllib2.Request(site, headers=hdr)
     page = urllib2.urlopen(req)
     from bs4 import BeautifulSoup
-    soup = BeautifulSoup(page,"html.parser")
+    soup = BeautifulSoup(page, "html.parser")
     soup.prettify()
-    for link in soup.findAll("div", { "class" : "cell name" }):
+    for link in soup.findAll("div", {"class": "cell name"}):
         if re.search(team, link.text, re.IGNORECASE):
-           text = re.split('(\d+)', link.text)
-           text = ' '.join(text)
+            text = re.split('(\d+)', link.text)
+            text = ' '.join(text)
     return text
-    
+
+
 def timee(city):
     text = "City not found"
     words = city.split()
-    if len(words)>=2:
+    if len(words) >= 2:
         city = city.split()[0] + "+" + city.split()[1]
-    site = "https://www.timeanddate.com/worldclock/?query="+city
+    site = "https://www.timeanddate.com/worldclock/?query=" + city
     hdr = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib2.Request(site,headers=hdr)
+    req = urllib2.Request(site, headers=hdr)
     page = urllib2.urlopen(req)
     from bs4 import BeautifulSoup
-    soup = BeautifulSoup(page,"html.parser")
+    soup = BeautifulSoup(page, "html.parser")
     soup.prettify()
-    for link in soup.findAll("td", { "class" : "rbi" }):
+    for link in soup.findAll("td", {"class": "rbi"}):
         text = link.text
         break
     return city + " - " + text
+
+
+def priceCall():
+    price = 0
+    site = "https://www.tomtop.com/p-pz0146b-eu.html"
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib2.Request(site, headers=hdr)
+    page = urllib2.urlopen(req)
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(page, "html.parser")
+    soup.prettify("utf-8")
+    for link in soup.findAll("span", {"class": "fz_orange pricelab"}):
+        price = link.text
+    return price
+
 
 def get_json_from_url(url):
     content = get_url(url)
     js = json.loads(content)
     return js
+
 
 def get_updates(offset=None):
     url = URL + "getUpdates?timeout=100"
@@ -85,7 +106,7 @@ def send_message(text, chat_id):
         url = URL + "sendMessage?text={}&chat_id={}".format("Hello there!", chat_id)
     elif "#weather" in text:
         words = text.split()
-        if len(words)>2:
+        if len(words) > 2:
             text = text.split()[1] + text.split()[2]
         else:
             text = text.split()[1]
@@ -95,7 +116,7 @@ def send_message(text, chat_id):
         url = URL + "sendMessage?text={}&chat_id={}".format(score(text), chat_id)
     elif "#time" in text:
         words = text.split()
-        if len(words)>2:
+        if len(words) > 2:
             text = text.split()[1] + " " + text.split()[2]
         else:
             text = text.split()[1]
@@ -104,16 +125,24 @@ def send_message(text, chat_id):
         url = URL + "sendMessage?text={}&chat_id={}".format(rollDice(), chat_id)
     elif "#coin" in text:
         url = URL + "sendMessage?text={}&chat_id={}".format(flipCoin().encode('utf-8'), chat_id)
+    elif "#price" in text:
+        newprice = 0
+        if newprice < priceCall():
+            newprice = priceCall()
+            url = URL + "sendMessage?text={}&chat_id={}".format(newprice, chat_id)
+        time.sleep(3600)
     else:
         url = URL + "sendMessage?text={}&chat_id={}".format("I`m stupid now...i don`t know much", chat_id)
     get_url(url)
-    
+
+
 def get_last_update_id(updates):
     update_ids = []
     for update in updates["result"]:
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
-    
+
+
 def echo_all(updates):
     for update in updates["result"]:
         try:
@@ -122,12 +151,13 @@ def echo_all(updates):
             send_message(text, chat)
         except Exception as e:
             print(e)
-            
+
+
 def main():
     last_update_id = None
     while True:
         updates = get_updates(last_update_id)
-        if len(updates["result"]) > 0:
+        if len(updates['result']) > 0:
             last_update_id = get_last_update_id(updates) + 1
             echo_all(updates)
         time.sleep(0.5)
